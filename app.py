e124967a86a0e060d10335792b2a7539424b68de
Yessica Lara <yessicalara2606@gmail.com>
2025-10-07 21:31:25 -0500

    Actualización: Cambios en código para crear histograma

--- app.py
+++ app.py
@@ -6,16 +6,9 @@ data = pd.read_csv('vehicles_us.csv')
 
 st.header('Aplicación web de analisis de datos de vehículos en EE.UU.')
 
-
-car_data = pd.read_csv('vehicles_us.csv')  # leer los datos
-hist_button = st.button('Construir histograma')  # crear un botón
-
-if hist_button:  # al hacer clic en el botón
-    # escribir un mensaje
-st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
-
-# crear un histograma
-fig = px.histogram(car_data, x="odometer")
-
-# mostrar un gráfico Plotly interactivo
-st.plotly_chart(fig, use_container_width=True)
+hist_button = st.button('Construir histograma')
+if hist_button:
+    st.write(
+        'Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
+    fig = px.histogram(data, x="odometer")
+    st.plotly_chart(fig, use_container_width=True)
