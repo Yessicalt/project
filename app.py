6bbdd31f902aefb81917a8f4846feacfa8630b6b
Yessica Lara <yessicalara2606@gmail.com>
2025-10-07 21:16:58 -0500

    Update: nuevo código para la app

--- app.py
+++ app.py
@@ -5,3 +5,17 @@ import plotly.express as px
 data = pd.read_csv('vehicles_us.csv')
 
 st.header('Aplicación web de analisis de datos de vehículos en EE.UU.')
+
+
+car_data = pd.read_csv('vehicles_us.csv')  # leer los datos
+hist_button = st.button('Construir histograma')  # crear un botón
+
+if hist_button:  # al hacer clic en el botón
+    # escribir un mensaje
+st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
+
+# crear un histograma
+fig = px.histogram(car_data, x="odometer")
+
+# mostrar un gráfico Plotly interactivo
+st.plotly_chart(fig, use_container_width=True)
